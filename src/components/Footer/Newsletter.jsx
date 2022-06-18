9d8351e1f9c2159968731cf52648b7a18e9dbfbf
Viachaslau Pailak <viachaslau.pailak@gmail.com>
2022-06-18 21:08:34 +0300

    feat: add some contentent and styles for components FooterOverlay and Newsletter

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subcribe to Our Newsletter</h1>
+      <p className="p__opensans">And Never Miss Latest Updates!</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter you e-mail address" />
+      <button className="custon__button">Subscribe</button>
+    </div>
   </div>
 );
 
